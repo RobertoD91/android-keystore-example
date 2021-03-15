d077ae5bf938ffedadff8cf96449d80ea546d3ce
Roberto <disantoroberto@gmail.com>
2021-03-15 19:48:28 +0100

    memo

--- app/src/main/java/com/example/keystore/MainActivity.java
+++ app/src/main/java/com/example/keystore/MainActivity.java
@@ -11,12 +11,16 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Timber.plant(new Timber.DebugTree());
 
-    String value = "Password/Token to be encrypted";
+    String value = "Password/Token to be encrypted"; // "Password/Token to be encrypted"
 
     String encryptedValue = EncryptionUtils.encrypt(this, value);
-    Timber.d(" Encrypted Value :" + encryptedValue);
+    Timber.d(" Encrypted Value :" + encryptedValue); // DwMVRTJ_4gSSPv39CXsw_dolsLNPYB7kQk1f4i4JuEGHM-3mC-h2ofiFhZ6JGQ==
 
     String decryptedValue = EncryptionUtils.decrypt(this, encryptedValue);
     Timber.d(" Decrypted Value :" + decryptedValue);
   }
 }
+
+// "Password/Token to be encrypted" DwMVRTJ_4gSSPv39CXsw_dolsLNPYB7kQk1f4i4JuEGHM-3mC-h2ofiFhZ6JGQ==
+// "Password/Token to be encrypted" DwMVRTJ_4gSSPv39CXsw_dolsLNPYB7kQk1f4i4JuEGHM-3mC-h2ofiFhZ6JGQ==
+// Pippozzo DwsWRipq6g9amwLNQKc_V5TSSbxHUc0Z
