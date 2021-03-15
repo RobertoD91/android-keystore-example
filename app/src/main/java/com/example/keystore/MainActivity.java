a2c822921eabe51325a102e3998aee9a6cf6ca8a
Roberto <disantoroberto@gmail.com>
2021-03-15 22:37:54 +0100

    popup e textview

--- app/src/main/java/com/example/keystore/MainActivity.java
+++ app/src/main/java/com/example/keystore/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.keystore;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,16 +15,22 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Timber.plant(new Timber.DebugTree());
 
-    String value = "Password/Token to be encrypted"; // "Password/Token to be encrypted"
+    String value = "pippozzo"; // "Password/Token to be encrypted"
 
     String encryptedValue = EncryptionUtils.encrypt(this, value);
-    Timber.d(" Encrypted Value :" + encryptedValue); // DwMVRTJ_4gSSPv39CXsw_dolsLNPYB7kQk1f4i4JuEGHM-3mC-h2ofiFhZ6JGQ==
+    Timber.d(" Encrypted Value :" + encryptedValue);
 
     String decryptedValue = EncryptionUtils.decrypt(this, encryptedValue);
     Timber.d(" Decrypted Value :" + decryptedValue);
+
+    Toast.makeText(this, "IN: "+value+"\n\nCript: "+encryptedValue+"Decript: "+decryptedValue, Toast.LENGTH_LONG).show();
+
+    TextView textView = (TextView) findViewById(R.id.textView);
+    textView.setText("Stringa in ingresso: "+value+"\nStringa Criptata: "+encryptedValue+"\nStringa Decriptata: "+decryptedValue);
   }
 }
 
-// "Password/Token to be encrypted" DwMVRTJ_4gSSPv39CXsw_dolsLNPYB7kQk1f4i4JuEGHM-3mC-h2ofiFhZ6JGQ==
+// risultati
 // "Password/Token to be encrypted" DwMVRTJ_4gSSPv39CXsw_dolsLNPYB7kQk1f4i4JuEGHM-3mC-h2ofiFhZ6JGQ==
 // Pippozzo DwsWRipq6g9amwLNQKc_V5TSSbxHUc0Z
+// pippozzo LwsWRipq6g_-2O62-GEOb4F6ysuElns7
